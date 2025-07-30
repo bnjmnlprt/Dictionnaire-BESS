1bbd6ad9938442149673a709e718240f45b3456e
bnjmnlprt <laporte.benjamin@gmail.com>
2025-07-30 21:06:59 +0200

    Update vite.config.ts V6

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 7a2dafca3d76f4f48a2931da079c8bbdd94ddf54
